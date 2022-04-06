304a5dc857da82f261bade656a23cea9021dbc80
Dani Coleman <danicoleman00@gmail.com>
2022-04-06 09:14:42 -0600

    Update create action taking out finding an item

--- app/controllers/manufacturer_items_controller.rb
+++ app/controllers/manufacturer_items_controller.rb
@@ -1,8 +1,8 @@
 class ManufacturerItemsController < ApplicationController
   def create
     manufacturer = Manufacturer.find(params[:new_manufacturer])
-    item = Item.find(params[:item_id])
-    ManufacturerItem.create!(item_id: item.id, manufacturer_id: manufacturer.id)
-    redirect_to "/items/#{item.id}"
+    # item = Item.find(params[:item_id])
+    ManufacturerItem.create!(item_id: params[:item_id], manufacturer_id: manufacturer.id)
+    redirect_to "/items/#{params[:item_id]}"
   end
 end
